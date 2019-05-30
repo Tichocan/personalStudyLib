db588a61ec6b8867c88c931830830849299bd490
Tichocan <31026298+Tichocan@users.noreply.github.com>
2019-05-30 10:05:31 +0800

    Add files via upload
    init

---
+++ pensonalStudyLib/cmake-build-debug/CMakeFiles/pensonalStudyLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pensonalStudyLib.dir/main.cpp.o"
+  "pensonalStudyLib.pdb"
+  "pensonalStudyLib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pensonalStudyLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
